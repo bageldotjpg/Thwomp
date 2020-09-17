55642e485e20a5475c5a7a4a311ad1eda9bf7f66
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-09-17 17:32:46 -0500

    Shy Guy improvements

--- src/main/java/com/bagel/thwomp/common/entity/ShyGuyEntity.java
+++ src/main/java/com/bagel/thwomp/common/entity/ShyGuyEntity.java
@@ -1,8 +1,16 @@
 package com.bagel.thwomp.common.entity;
 
+import com.bagel.thwomp.core.registry.ThwompEntities;
+import com.bagel.thwomp.core.registry.ThwompItems;
+
+import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.FoxEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.IPacket;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class ShyGuyEntity extends FoxEntity {
 
@@ -10,4 +18,18 @@ public class ShyGuyEntity extends FoxEntity {
 		super(type, worldIn);
 	}
 
+	@Override
+	public ItemStack getPickedResult(RayTraceResult target) {
+		return new ItemStack(ThwompItems.SHY_GUY_SPAWN_EGG.get());
+	}
+	
+	@Override
+    public ShyGuyEntity createChild(AgeableEntity ageable) {
+        return ThwompEntities.SHY_GUY.get().create(this.world);
+    }
+
+	@Override
+	public IPacket<?> createSpawnPacket() {
+		return NetworkHooks.getEntitySpawningPacket(this);
+	}
 }
